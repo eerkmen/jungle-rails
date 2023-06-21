f116f2db06cf8f776a4c67d1da31c04780f89bfb
eerkmen <efeerkmen@gmail.com>
2023-06-21 00:00:00 -0400

    add function basic to dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+  
+
+  def basic
+    authenticate_or_request_with_http_basic do |id, password|
+      id == ENV['BASIC_AUTH_ID'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+  
   def show
     @products = Product.all
     @categories = Category.all
