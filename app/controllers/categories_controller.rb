e1f38472b553c8250f451c853abdf6a7a135d5f0
eerkmen <efeerkmen@gmail.com>
2023-06-26 00:00:00 -0400

    remove show function from categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,3 @@
-class CategoriesController < ApplicationController
-
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
-  end
-
+class Admin::CategoriesController < ApplicationController
+ 
 end
