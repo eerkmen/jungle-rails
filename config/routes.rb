7f589a01a713117127537ae73b3909a2704217f8
eerkmen <efeerkmen@gmail.com>
2023-07-02 00:00:00 -0400

    add GET logout to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get '/about', to: 'about#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
