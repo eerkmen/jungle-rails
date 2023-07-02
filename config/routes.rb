9c457294a6002512f47a3d1dc8d9a6781c6b1f1d
eerkmen <efeerkmen@gmail.com>
2023-07-02 00:00:00 -0400

    add POST login to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
   get '/about', to: 'about#show'
   get '/login' => 'sessions#new'
- 
+  post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
