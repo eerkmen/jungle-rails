0d0a800d5c8d119ce153a173e581a976cd08639e
eerkmen <efeerkmen@gmail.com>
2023-07-03 09:05:26 +0000

    add categories to controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
   get '/about', to: 'about#show'
   # The priority is based upon order of creation: first created -> highest priority.
