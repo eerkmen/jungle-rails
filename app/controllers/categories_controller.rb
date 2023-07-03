af6e9eab929fb848b890e70bdb2871330d2f72da
eerkmen <efeerkmen@gmail.com>
2023-07-03 09:50:29 +0000

    add feature user authentication

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,8 @@
-class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+class CategoriesController < ApplicationController
 
-  def index
-    @categories = Category.order(id: :desc).all
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
   end
 
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-
-    if @category.save
-      redirect_to [:admin, :categories], notice: 'categories created!'
-    else
-      render :new
-    end
-  end
-
-  def category_params
-    params.require(:category).permit(
-      :name
-    )
-  end
 end
