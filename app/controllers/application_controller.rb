73514e44e19d4f9e305a575a60d816fa7bc0b5d8
eerkmen <efeerkmen@gmail.com>
2023-07-03 10:26:25 +0000

    add feature session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  
   private
 
   def cart
@@ -28,5 +29,14 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  def current_user
+     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
   end
 end
