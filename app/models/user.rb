6f4b8f067bad3e44d7c11e51de49d457ccc16aa3
eerkmen <efeerkmen@gmail.com>
2023-07-04 00:00:00 -0400

    add authenticate with credidentials func to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: true
+  
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      user
+    end
+  end
 end
