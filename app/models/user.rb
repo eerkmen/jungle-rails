4851ffbf746b9600caabeebd19d5c0b19ca5bc9c
eerkmen <efeerkmen@gmail.com>
2023-07-04 00:00:00 -0400

    add validates statements to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  
+  validates :password, confirmation: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+
   def self.authenticate_with_credentials(email, password)
     user = User.find_by_email(email.strip.downcase)
     # If the user exists AND the password entered is correct.
